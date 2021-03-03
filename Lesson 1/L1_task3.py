6752bb12571d8aeb18a96e959f5b0b331e644b73
MikhailRozov <shkin29@rambler.ru>
2021-03-03 14:22:35 +0300

    Lesson 1 hometask

---
+++ Lesson 1/L1_task3.py
@@ -0,0 +1,6 @@
+# Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
+# Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
+
+number = input('Введите число: ')
+answer = int(number) + int(number + number) + int(number + number + number)
+print(f'Сумма {number} + {number}{number} + {number}{number}{number} будет равна {answer}')
